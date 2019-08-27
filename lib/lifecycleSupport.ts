a1eeabb38cd6fce4afb2298797c57dacf065ab0a
Christian Dupuis <cd@atomist.com>
2019-08-27 12:31:16 +0200

    Move below goals

--- lib/lifecycleSupport.ts
+++ lib/lifecycleSupport.ts
@@ -284,8 +284,8 @@ export const DefaultLifecycleRenderingOptions: LifecycleOptions = {
             renderers: [() => [
                 new pr.PushNodeRenderer(),
                 new pr.CommitNodeRenderer(),
-                new sr.ComplianceNodeRenderer(),
                 new sr.GoalSetNodeRenderer(),
+                new sr.ComplianceNodeRenderer(),
                 new sr.StatusesNodeRenderer(),
                 new WorkflowNodeRenderer(),
                 new pr.IssueNodeRenderer(),
