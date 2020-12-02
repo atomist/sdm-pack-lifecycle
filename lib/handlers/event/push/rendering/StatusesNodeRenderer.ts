3c9f37ed7e8ddc0e16a19b667ce3bd66de772d58
Christian Dupuis <cd@atomist.com>
2020-12-02 12:25:12 +0100

    Link for checks from footer

--- lib/handlers/event/push/rendering/StatusesNodeRenderer.ts
+++ lib/handlers/event/push/rendering/StatusesNodeRenderer.ts
@@ -140,8 +140,8 @@ export class StatusesNodeRenderer extends AbstractIdentifiableContribution
             pending > 0 ? "#2A7D7D" :
                 error > 0 ? "#BC3D33" :
                     "#37A745";
-
-        const summary = summarizeStatusCounts(pending, success, error);
+        const checkUrl = _.sortBy(checks, "url")[0].url;
+        const summary = url(checkUrl, summarizeStatusCounts(pending, success, error));
 
         if (lines.length > 0) {
             const attachment: Attachment = {
